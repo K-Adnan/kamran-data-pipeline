26384468bab75b0d9e8b126bef5ee62b7249272a
Kamran Adnan <32751325+K-Adnan@users.noreply.github.com>
2024-11-14 11:53:53 +0000

    Transaction emitter completed

--- data-emitter/src/main/scala/dataemitter/DataEmitter.scala
+++ data-emitter/src/main/scala/dataemitter/DataEmitter.scala
@@ -1,9 +1,10 @@
 package dataemitter
 
-import ExampleData.{firstNamesSource, lastNamesSource}
 import akka.actor.ActorSystem
 import akka.stream.Materializer
 import akka.stream.scaladsl.{Flow, Sink, Source}
+import dataemitter.ExampleData._
+
 import scala.concurrent.duration._
 
 object DataEmitter extends App {
@@ -11,10 +12,20 @@ object DataEmitter extends App {
   implicit val system: ActorSystem = ActorSystem("data-emitter")
   implicit val mat: Materializer = Materializer(system)
 
-  private val nameCombiner: Source[String, _] = firstNamesSource.zip(lastNamesSource)
-    .map {
-      case (firstName, lastName) => s"$firstName $lastName"
-    }.throttle(5, 1.second)
+  private val combinedSource =
+    Source.zipN(Seq(timeSource, firstNamesSource, lastNamesSource, phoneNumberSource, transactionId, transactionAmountSource))
 
-  nameCombiner.runWith(Sink.foreach(println))
+  private val transactionFlow: Flow[Seq[Any], Transaction, _] =
+    Flow[Seq[Any]].map { s =>
+      Transaction(
+        s(0).asInstanceOf[Long],
+        s(1).asInstanceOf[String],
+        s(2).asInstanceOf[String],
+        s(3).asInstanceOf[String],
+        s(4).asInstanceOf[Long],
+        s(5).asInstanceOf[Double]
+      )
+    }
+
+  combinedSource.throttle(5, 1.second).via(transactionFlow).runWith(Sink.foreach(println))
 }
--- data-emitter/src/main/scala/dataemitter/ExampleData.scala
+++ data-emitter/src/main/scala/dataemitter/ExampleData.scala
@@ -1,8 +1,8 @@
 package dataemitter
 
-import akka.NotUsed
 import akka.stream.scaladsl.Source
 
+import java.time.Duration
 import scala.util.Random
 
 object ExampleData {
@@ -14,8 +14,27 @@ object ExampleData {
     firstNames(Random.nextInt(firstNames.size))
   }
 
-  val lastNamesSource: Source[String, NotUsed] = Source.repeat(()).map { _ =>
+  val lastNamesSource: Source[String, _] = Source.repeat(()).map { _ =>
     lastNames(Random.nextInt(lastNames.size))
+  }
+
+  private val currentTimeMillis = System.currentTimeMillis()
+  private val oneYearAgoMillis = currentTimeMillis - Duration.ofDays(365).toMillis
+
+  val timeSource: Source[Long, _] = Source.repeat(()).map { _ =>
+    oneYearAgoMillis + Random.nextLong(Duration.ofDays(365).toMillis)
+  }
+
+  val transactionId: Source[Long, _] = Source.repeat(()).map { _ =>
+    Random.nextLong(99999999999L) + 20300000000000L
+  }
+
+  val phoneNumberSource: Source[String, _] = Source.repeat(()).map { _ =>
+    "07" + Random.nextLong(999999999)
+  }
+
+  val transactionAmountSource: Source[Double, _] = Source.repeat(()).map { _ =>
+    BigDecimal(Random.between(1.99, 200)).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
   }
 
   private def fileToSeq(path: String): Seq[String] = {
@@ -24,4 +43,13 @@ object ExampleData {
     source.close
     seq
   }
+
+  case class Transaction(
+      transactionTime: Long,
+      firstName: String,
+      lastName: String,
+      phoneNumber: String,
+      transactionId: Long,
+      transactionAmount: Double
+  )
 }
